ba080f70df16b59f8b4a044c3552ea43f0f1e85c
TheAverageRush <livewithy2j@rocketmail.com>
2020-12-02 23:56:47 +0500

    Added Health Status Menu

---
+++ health_status/health_status.py
@@ -0,0 +1,27 @@
+from config import health_status_number_of_choices
+
+def health_main():
+    while True:
+        print(""" 
+        Welcome to the Health Status Menu
+        ----------------------
+        * Enter [1] to check for Health Status
+        * Enter [2] to go back
+        ----------------------
+        """)
+
+        choice = int(input(">   "))
+        if choice not in health_status_number_of_choices:
+            print("----------------")
+            print("Please enter a valid choice!")
+            print("----------------")
+
+        else:
+            if choice == 1:
+                print("Choice 1")
+
+
+            elif choice == 2:
+                print("Going back...")
+                print("------------------------")
+                break
