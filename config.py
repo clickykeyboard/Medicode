1f3461d92c83c1cb0cbc6a7be001707e6b37039b
TheAverageRush <livewithy2j@rocketmail.com>
2020-12-03 13:37:23 +0500

    Dealing with error, pushing it for correction.

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ class Configuration:
     main_number_of_choices = [1, 2, 3, 4, 5, 6, 7, 8]
     medical_main_number_of_choices = [1, 2]
     health_status_number_of_choices = [1, 2]
+    emergency_status_number_of_choices = [1, 2]
 
     active_patient = {
 
---
+++ emergency/emergency.py
@@ -0,0 +1,45 @@
+from config import Configuration
+
+class Emergency:
+    def __init__(self,emergency_history, room_number):
+        self.emergency_history = emergency_history
+        self.room_number = room_number
+
+    def get_emergency_history(self):
+        return self.emergency_history
+
+    def get_room_number(self):
+        return self.room_number
+
+    def set_emergency_history(self, new_emergency_history):
+        self.emergency_history = new_emergency_history
+
+    def set_room_number(self, new_room_number):
+        self.room_number = new_room_number
+
+
+def emergency_main():
+    while True:
+        print(""" 
+        Welcome to the Emergency Status Menu
+        ----------------------
+        * Enter [1] to check for Emergency Status
+        * Enter [2] to go back
+        ----------------------
+        """)
+
+        choice = int(input(">   "))
+        if choice not in Configuration.emergency_status_number_of_choices:
+            print("----------------")
+            print("Please enter a valid choice!")
+            print("----------------")
+
+        else:
+            if choice == 1:
+                print("Choice 1")
+
+
+            elif choice == 2:
+                print("Going back...")
+                print("------------------------")
+                break
