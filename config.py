c9c7bf517b1433f2bfd2c72af52cc06580af681f
TheAverageRush <livewithy2j@rocketmail.com>
2020-12-06 17:22:11 +0500

    Updated config.py

--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 class Configuration:
 
     main_number_of_choices = [1, 2, 3, 4, 5, 6, 7]
-    medical_main_number_of_choices = [1, 2]
-    health_status_number_of_choices = [1, 2]
-    emergency_status_number_of_choices = [1, 2]
-    blood_status_number_of_choices = [1, 2, 3]
+    patient_main_number_of_choices = [1, 2]
+    medical_history_number_of_choices = [1, 2]
+    blood_section_number_of_choices = [1, 2,]
+    schedule_appointment_number_of_choices = [1, 2]
+    covid_19_number_of_choices = [1, 2,]
+    emergency_number_of_choices = [1, 2]
+
 
     active_patient = {
 
